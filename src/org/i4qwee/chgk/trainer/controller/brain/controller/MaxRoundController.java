e845c4acbbdd0d9d4ecf76b9172df98615a00246
4qwee <tmp@email.ru>
2012-02-04 09:42:55 +0200

    fixed bug with new game

--- src/org/i4qwee/chgk/trainer/controller/brain/controller/MaxRoundController.java
+++ src/org/i4qwee/chgk/trainer/controller/brain/controller/MaxRoundController.java
@@ -1,10 +1,13 @@
 package org.i4qwee.chgk.trainer.controller.brain.controller;
 
 import org.i4qwee.chgk.trainer.controller.brain.ScoreManagerSingleton;
+import org.i4qwee.chgk.trainer.controller.brain.listener.GameStateListener;
 import org.i4qwee.chgk.trainer.controller.brain.listener.RoundListener;
+import org.i4qwee.chgk.trainer.controller.brain.manager.GameStateManager;
 import org.i4qwee.chgk.trainer.controller.brain.manager.NamesManager;
 import org.i4qwee.chgk.trainer.controller.brain.manager.RoundManager;
 import org.i4qwee.chgk.trainer.controller.brain.manager.ScoreManager;
+import org.i4qwee.chgk.trainer.model.enums.GameState;
 import org.i4qwee.chgk.trainer.view.dialogs.MessageDialog;
 
 import javax.swing.*;
@@ -14,7 +17,7 @@ import javax.swing.*;
  * Date: 06.01.12
  * Time: 13:08
  */
-public class MaxRoundController implements RoundListener
+public class MaxRoundController implements GameStateListener
 {
     private static final MaxRoundController instance = new MaxRoundController();
 
@@ -22,10 +25,11 @@ public class MaxRoundController implements RoundListener
     private final ScoreManager scoreManager = ScoreManager.getInstance();
     private final NamesManager namesManager = NamesManager.getInstance();
     private final MessageDialog messageDialog = MessageDialog.getInstance();
+    private final GameStateManager gameStateManager = GameStateManager.getInstance();
 
     private MaxRoundController()
     {
-        roundManager.addListener(this);
+        gameStateManager.addListener(this);
     }
 
     public static MaxRoundController getInstance()
@@ -33,9 +37,9 @@ public class MaxRoundController implements RoundListener
         return instance;
     }
 
-    public void onRoundChanged(int round, int maxRound)
+    public void onGameStageChanged(GameState gameState)
     {
-        if (maxRound != 0 && round > maxRound)
+        if (roundManager.getMaxRound() != 0 && roundManager.getRound() >= roundManager.getMaxRound() && gameState == GameState.FINISHED)
         {
             if (scoreManager.getLeftScore() > scoreManager.getRightScore())
                 messageDialog.show(namesManager.getLeftName() + " выиграл!");
